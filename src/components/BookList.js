e5fc7c46abe6603d484eb177329deacc1397ce9a
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-06-24 14:22:25 +0100

    Accessing Context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
     
+    static  contextType = ThemeContext;
     render() { 
+        const {dark,light,isLightTheme}  =this.context;
+        const theme = isLightTheme ? light : dark;
         return ( 
-            <div className="book-list">
+            <div className="book-list" style={{background : theme.bg,color:theme.syntax}}>
                   <ul>
-                    <li>The way of the kings </li>
-                    <li>The name of the wind</li>
-                    <li>the final empire</li>
+                    <li style={{background : theme.ui}}>The way of the kings </li>
+                    <li style={{background : theme.ui}}>The name of the wind</li>
+                    <li style={{background : theme.ui}}>the final empire</li>
                 </ul>
             </div>
          );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
     
+    static contextType = ThemeContext;
     render() { 
+       const {dark,light,isLightTheme}  =this.context;
+       const theme = isLightTheme ? light : dark;
         return (
-            <nav>
+            <nav style={{background : theme.ui,color:theme.syntax}}>
                 <h1>Conetxt App</h1>
                 <ul>
                     <li>Home </li>
