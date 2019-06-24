d687a895aef6e2973cdfa4313d3550f377dc0d55
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-06-24 15:00:59 +0100

    Consuming Multiple Contexts  AuthContext

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import BookList from './components/BookList';
-import  ThemeContextProvider  from './contexts/ThemeContext';
-import ThemeToggle from './components/ThemeToggle';
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import BookList from "./components/BookList";
+import ThemeContextProvider from "./contexts/ThemeContext";
+import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/authContext";
 
 function App() {
   return (
     <div className="App">
-    <ThemeContextProvider>
-    <Navbar />
-    <BookList />
-    <ThemeToggle />
-    </ThemeContextProvider>
-    
-     
+      <ThemeContextProvider>
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
+      </ThemeContextProvider>
     </div>
   );
 }
